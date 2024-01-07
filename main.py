6dfe4c70cf7a8aef78e8b2616e31d56baacf0726
Gabriel Carroll <gabrielcarroll11@gmail.com>
2024-01-07 07:23:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import Tkinter and modules
 import tkinter as tk
 from tkinter import Button
+from tkinter import Label
 
 # Import custom Tkinter and modules
 import customtkinter
@@ -10,8 +11,12 @@ from customtkinter import CTkEntry
 # Create the store passwords function
 def store():
     with open ("Passwords.txt", "a") as passwrds:
-        passwrds.write ("" + site.get() + " | " + password.get() + " \n") 
-     
+        passwrds.write ("" + site.get() + " | " + password.get() + " \n") # Use .get() to retrive the variable
+
+# Create the fuction for showing passwords
+def display():
+    pass
+    
 # Set the colour scheme
 customtkinter.set_appearance_mode("system") # Colour is set to system so when it changes when the user changes there colour scheme
 customtkinter.set_default_color_theme("blue")
@@ -19,14 +24,18 @@ customtkinter.set_default_color_theme("blue")
 root = customtkinter.CTk()
 
 # Set the windows size
-root.geometry("400x320")
+root.geometry("400x450")
 
 # Set the windows title
 root.title("CryptoniteKeeper")
 
-# Add a button
-text = CTkButton(root, text = "Store", command = store, corner_radius=55)
-text.pack (padx=10, pady=10)
+# Add a button to show the passwords
+show = CTkButton(root, text="Show Passwords", corner_radius=55, command = display,)
+show.pack(padx=10, pady = 10)
+
+# Add a button to save the password
+save = CTkButton(root, text = "Store", command = store, corner_radius=55)
+save.pack (padx=10, pady=10)
 
 # Add a textbox where user would input the site or service for the password
 site = tk.StringVar()
